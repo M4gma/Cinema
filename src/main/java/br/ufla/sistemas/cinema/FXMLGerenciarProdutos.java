9a96dbb1346b420ebcde5aaff694aba21a8bb1aa
M4gma <lucio.fleury@estudante.ufla.br>
2019-06-15 19:02:13 -0300

    Botões podem modificar o programa principal. O botão GerenciarProdutos faz isso

---
+++ src/main/java/br/ufla/sistemas/cinema/FXMLGerenciarProdutos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufla.sistemas.cinema;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author lucio
+ */
+public class FXMLGerenciarProdutos implements Initializable{
+
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
